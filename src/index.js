b055a314cec90c5c9efa076c06c23a9ba5a9a9a0
SelinaMangaroo <selinamangaroo@gmail.com>
2020-07-08 14:25:23 -0400

    updates

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import FrontPage from "./FrontPage";
 ReactDOM.render(
   <React.StrictMode>
     <FrontPage
-      mode="interactive-sets"
+      mode="grid"
       endpoint="http://metabolic2.whirl-i-gig.com:8085/Front/getData/download/1"
     />
   </React.StrictMode>,
